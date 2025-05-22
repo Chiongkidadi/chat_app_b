1be18d1f23a6d9a1e19bebe71b887d893566c5b6
Chiongkidadi <chiongysha@gmail.com>
2025-05-22 16:02:09 +0800

    Building dynamic lists

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -24,17 +24,18 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView(
-              children: [
-                ChatBubble(
-                  alignment: Alignment.centerLeft,
+            //TODO: Create a dynamic sized list
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ChatBubble(
+                  alignment:
+                      index % 2 == 0
+                          ? Alignment.centerLeft
+                          : Alignment.centerRight,
                   message: "Hello, this is Pooja!",
-                ),
-                ChatBubble(
-                  alignment: Alignment.centerRight,
-                  message: "Hello, this is Pooja!",
-                ),
-              ],
+                );
+              },
             ),
           ),
           ChatInput(),
