fc33316b351b573539e2e4a6dee321f566c7ed1e
Chiongkidadi <chiongysha@gmail.com>
2025-05-23 09:48:22 +0800

    Organizing the styles and colors for the chat page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/utils/spaces.dart';
 import 'package:chat_app/utils/textfield_styles.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
@@ -78,7 +79,7 @@ class LoginPage extends StatelessWidget {
                       },
                       controller: userNameController,
                     ),
-                    SizedBox(height: 24),
+                    verticalSpacing(24),
                     LoginTextField(
                       hasAsterisks: true,
                       controller: passwordController,
@@ -87,7 +88,7 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 24),
+              verticalSpacing(24),
               ElevatedButton(
                 onPressed: () {
                   loginUser(context);
