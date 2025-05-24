dd7aa831efff9dd8af1bb930c310bc7c87bce6fb
Chiongkidadi <chiongysha@gmail.com>
2025-05-24 19:04:17 +0800

    Modeling entities in dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
@@ -16,31 +17,30 @@ class ChatPage extends StatelessWidget {
         title: Text('Hi $username!'),
         actions: [
           IconButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/');
-              print('Icon pressed!');
-            },
-            icon: Icon(Icons.logout),
-          ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/');
+                print('Icon pressed!');
+              },
+              icon: Icon(Icons.logout))
         ],
       ),
       body: Column(
         children: [
           Expanded(
-            //TODO: Create a dynamic sized list
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return ChatBubble(
-                  alignment:
-                      index % 2 == 0
-                          ? Alignment.centerLeft
-                          : Alignment.centerRight,
-                  message: "Hello, this is Pooja!",
-                );
-              },
-            ),
-          ),
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return ChatBubble(
+                        alignment: index % 2 == 0
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
+                        //TODO: Add more chat & author related properties here
+                        entity: ChatMessageEntity(
+                            id: '1234',
+                            text: 'Hello this is Pooja!!!!!',
+                            createdAt: DateTime.now().millisecondsSinceEpoch,
+                            author: Author(userName: 'poojab26')));
+                  })),
           ChatInput(),
         ],
       ),
--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -1,11 +1,12 @@
+import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
-  final String message;
+  final ChatMessageEntity entity;
   final Alignment alignment;
 
-  const ChatBubble({Key? key, required this.alignment, required this.message})
-    : super(key: key);
+  const ChatBubble({Key? key, required this.alignment, required this.entity})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,24 +18,23 @@ class ChatBubble extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '$message',
+              '${entity.text}',
               style: TextStyle(fontSize: 20, color: Colors.white),
             ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 200,
-            ),
+            if (entity.imageUrl != null)
+              Image.network(
+                '${entity.imageUrl}',
+                height: 200,
+              )
           ],
         ),
         margin: EdgeInsets.all(50),
         decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(12),
-            topRight: Radius.circular(12),
-            bottomLeft: Radius.circular(12),
-          ),
-        ),
+            color: Colors.grey,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12))),
       ),
     );
   }
